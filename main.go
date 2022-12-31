e5cdbdc99096cf7a500a49c93c6470e8f63c3520
yosnak13 <yosnak13@gmail.com>
2022-12-31 14:18:30 +0900

    CommandLine引数のURLをQRへ変換

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image/png"
 	"os"
@@ -20,6 +21,13 @@ var favicon = []byte{
 }
 
 func main() {
+	flag.Parse()
+	url := flag.Arg(0)
+	if url == "" {
+		fmt.Println("URL is empty")
+		return
+	}
+
 	file, err := os.Create("./qr.png")
 	if err != nil {
 		fmt.Printf("file generation failed: %v\n", err)
@@ -27,7 +35,7 @@ func main() {
 	}
 	defer file.Close()
 
-	img, err := qrgen.GenQRCode("https://www.google.com")
+	img, err := qrgen.GenQRCode(url)
 	if err != nil {
 		fmt.Printf("image generation failed: %v\n", err)
 		return
